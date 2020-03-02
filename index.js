5b9fc77013ffddc9b78422ae14e93b0bb20d9d96
mathmammoth <60290221+mathmammoth@users.noreply.github.com>
2020-03-02 14:05:13 +0300

    eleventh

---
+++ index.js
@@ -0,0 +1,28 @@
+function isEmail() {
+    let str = document.getElementById("email").value;
+    let re = /^[^\s()<>@,;:\/]+@\w[\w\-]+\.[a-z]{2,}$/i;
+    if (re.test(str)) status.innerHTML = alert('Спасибо за подписку!');
+    else status.innerHTML = alert('Надо ввести почту!)');
+    if (isEmpty(str)) status.innerHTML = alert('Вы ничего не ввели...');
+}
+function isEmpty(str){
+    return (str === null) || (str.length === 0);
+}
+
+function isMessage() {
+    let name = document.getElementById("name").value;
+    let your = document.getElementById("your_mail").value;
+    let text = document.getElementById("text").value;
+    let re = /^[[а-яА-ЯёЁ]]{2,64}$/;
+    let de = /^[^\s()<>@,;:\/]+@\w[\w\-]+\.[a-z]{2,}$/i;
+    if (re.test(name)) status.innerHTML = true;
+    else if (de.test(your)) status.innerHTML = true;
+    else status.innerHTML = alert('Надо ввести почту!)');
+    if (re.test(text)) status.innerHTML = alert('Спасибо за ваш отзыв!');
+    else status.innerHTML = alert('У вас пустое сообщение');
+    if (isEmptey(your)) status.innerHTML = alert('Заполните форму!');
+}
+
+function isEmptey(your){
+    return (your === null) || (your.length === 0);
+}
